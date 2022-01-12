86c0423a2032cc9f610b9bea0831a20a233259c0
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-01-12 19:12:56 -0300

    create 01-comment

---
+++ content/01-comment/01-comment.js
@@ -0,0 +1,6 @@
+// * Comentarios em JavaScript
+//#>> Existem dois tipos:
+    // $> inline - usando (//) <= duas barras;
+    // $> multline - usando (/**/) <= usando desse mesmo jeito;
+
+// Hello World!!
