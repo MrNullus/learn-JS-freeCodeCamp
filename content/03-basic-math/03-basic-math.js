053f41be9f8896a99b4685daeb82ab1508238e4c
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-01-12 19:15:04 -0300

    create 03-basic-math

---
+++ content/03-basic-math/03-basic-math.js
@@ -0,0 +1,36 @@
+/*
+    & Matematica basica &
+$*/
+/* - Operações Basicas  - */
+//* Adição
+var sum = 12 + 24;
+console.log(sum)
+
+//* Subtração
+var difference = 12 - 101212.221;
+console.log(difference);
+
+//* Multiplicação
+var product = 11212.222323 * 1.78;
+console.log(product);
+
+//* Divisão
+var quotient = 0.232322312 / 0.121234;
+console.log(quotient);
+
+//* Resto
+var remainder;
+remainder = 0.12123356 % 0.15;
+
+/* - Incremento & Decremento - */
+// Incremento -> aumentar um número;
+var myVarIncre = 90;
+myVarIncre = myVarIncre + 1; // forma completa
+myVarIncre +=  1 // forma meia boca
+myVarIncre++; // forma suprema
+
+// Decremento -> diminuir um número;
+var myVarDecre = 3.3;
+myVarDecre = myVarDecre - 0.023; // forma completa
+myVarDecre -=  1.1111 // forma meia boca
+myVarDecre--; // forma suprema
